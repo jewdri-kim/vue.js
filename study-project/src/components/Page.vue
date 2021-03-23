ff62814577582f990b88964b4ce850dd3b302a13
jewdri <ksoldier86@gmail.com>
2021-03-23 12:52:46 +0900

    라우터

--- study-project/src/components/Page.vue
+++ study-project/src/components/Page.vue
@@ -4,7 +4,7 @@
         <nav>
             <ul>
                 <li><v-link href="/">웰컴</v-link></li>
-                <li><v-link href="/about">어바웃</v-link></li>
+                <li><v-link href="/about">어바웃 </v-link></li>
                 <li><v-link href="/page">페이지</v-link></li>
             </ul>
         </nav> 
